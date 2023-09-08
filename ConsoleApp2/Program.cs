66a80c38ad4c00c2e5c4777421ff457a7af6056f
Admin <Admin@TEB123003>
2023-09-08 15:12:25 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace uppgift3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Clear();
+            Console.WriteLine("vlad har övat ovh gjort klar uppgiften ");
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" /    \\");
+            Console.WriteLine("/______\\");
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" vladut "+" ösian ");
+            Console.WriteLine(" /    \\");
+            Console.WriteLine("/______\\");
+            Console.ReadKey();
+        }
+    }
+}
